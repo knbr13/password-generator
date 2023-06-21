7d3b8aa0afb0d0f7a67b064f9033650e265317ca
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-21 09:33:05 +0300

    create PasswordStrengthCalculator class

---
+++ PasswordStrengthCalculator.cs
@@ -0,0 +1,9 @@
+namespace PasswordGenerator
+{
+    static class PasswordStrengthCalculator
+    {
+        static string WeakPassword = "Weak";
+        static string ModeratePassword = "Moderate";
+        static string StrongPassword = "Strong";
+    }
+}
