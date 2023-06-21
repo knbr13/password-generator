acfae37a481da8707e3b0eea7aceb52d46ad2e70
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-21 09:38:26 +0300

    implement 'PasswordStrength' method

--- PasswordStrengthCalculator.cs
+++ PasswordStrengthCalculator.cs
@@ -5,5 +5,23 @@ namespace PasswordGenerator
         static string WeakPassword = "Weak";
         static string ModeratePassword = "Moderate";
         static string StrongPassword = "Strong";
+
+        static string PasswordStrength(
+            int length,
+            bool useUpperCase,
+            bool useLowerCase,
+            bool useSpecialChars,
+            bool useNumbers
+        )
+        {
+            if (length < 8 || (!useUpperCase || !useLowerCase || !useNumbers || !useSpecialChars))
+                return WeakPassword;
+            else if (
+                length < 12 || (!useUpperCase && !useLowerCase) || (!useNumbers && !useSpecialChars)
+            )
+                return ModeratePassword;
+            else
+                return StrongPassword;
+        }
     }
 }
