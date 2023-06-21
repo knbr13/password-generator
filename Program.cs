61e538140047d4957f520d6a9ad1532c678ac543
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-21 03:44:27 +0300

    implement 'GetPasswordLength' method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PasswordGenerator
+{
+    class Program
+    {
+        static int GetPasswordLength()
+        {
+            int passwordLength;
+            while (true)
+            {
+                Console.Write("Enter the length of the password: ");
+                if (int.TryParse(Console.ReadLine(), out passwordLength) && passwordLength > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+            }
+            return passwordLength;
+        }
+    }
+}
