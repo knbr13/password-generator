18025d17041268041a4eebb7f0f823b246a7dbf4
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-28 17:53:54 +0300

    add option functions

--- password.go
+++ password.go
@@ -28,3 +28,33 @@ func New(options ...Option) *Password {
 	}
 	return p
 }
+
+func WithLowerCaseChars(b bool) Option {
+	return func(p *Password) {
+		p.WithLowerCaseChars = b
+	}
+}
+
+func WithUpperCaseChars(b bool) Option {
+	return func(p *Password) {
+		p.WithUpperCaseChars = b
+	}
+}
+
+func WithDigitsChars(b bool) Option {
+	return func(p *Password) {
+		p.WithDigitsChars = b
+	}
+}
+
+func WithSpecialChars(b bool) Option {
+	return func(p *Password) {
+		p.WithSpecialChars = b
+	}
+}
+
+func OfStrength(s Strength) Option {
+	return func(p *Password) {
+		p.Strength = s
+	}
+}
